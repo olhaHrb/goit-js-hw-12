3567f6675a142dc41d0ce5270cbc092e989d9d89
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2024-05-19 14:35:56 +0300

    add axios

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,12 +1,15 @@
+import axios from "axios";
+
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '43873427-52524e51f424820d3ed845203';
 const params = `?key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true`;
 
 export function fetchImg(searchquery) {
-  return fetch(`${BASE_URL}${params}&q=${searchquery}`).then(response => {
-    if (!response.ok) {
-      throw new Error();
-    }
-    return response.json();
-  });
+    return axios.get(`${BASE_URL}${params}&q=${searchquery}`)
+        .then(response => {
+            return response.data;
+        })
+        .catch(error => {
+            console.log(error);
+        });
 };
