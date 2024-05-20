5d537d392341194b6009212c2ed2f53616d3b2c3
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2024-05-20 11:43:11 +0300

    add async await

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,12 +4,7 @@ const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '43873427-52524e51f424820d3ed845203';
 const params = `?key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true`;
 
-export function fetchImg(searchquery) {
-    return axios.get(`${BASE_URL}${params}&q=${searchquery}`)
-        .then(response => {
-            return response.data;
-        })
-        .catch(error => {
-            console.log(error);
-        });
+export async function fetchImg(searchquery) {
+    const response = await axios.get(`${BASE_URL}${params}&q=${searchquery}`)
+        return response.data;
 };
