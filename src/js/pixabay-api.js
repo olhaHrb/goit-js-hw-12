99ebcffc1cea32c7dfd67ef7755e13d72cb15bdb
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2024-05-21 11:06:19 +0300

    btn Load more

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '43873427-52524e51f424820d3ed845203';
-const params = `?key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true`;
+import { PAGE } from "../main";
+const PER_PAGE = 15;
+const params = `?key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true&page=${PAGE}&per_page=${PER_PAGE}`;
 
 export async function fetchImg(searchquery) {
     const response = await axios.get(`${BASE_URL}${params}&q=${searchquery}`)
--- src/main.js
+++ src/main.js
@@ -3,18 +3,27 @@ import { renderImageCard } from "./js/render-functions";
 
 const loader = document.querySelector('.loader');
 const formEach = document.querySelector(".form");
+const btnLoad = document.querySelector('.btn_load');
+let searchValue = "";
+let pageValue;
+export const PAGE = pageValue;
+
 formEach.addEventListener("submit", onSearch);
+btnLoad.addEventListener("click", onLoad)
 
 function onSearch(event) {
   event.preventDefault();
   loader.classList.remove('is-hidden');
+  pageValue = 1;
   const form = event.currentTarget;
   const searchQuery = form.elements.searchInput.value.trim();
 
   if (searchQuery === "") { 
     loader.classList.add('is-hidden');
     return;
-  };   
+  };  
+  
+  searchValue = searchQuery;
 
   fetchImg(searchQuery)
     .then(image => {
@@ -26,10 +35,26 @@ function onSearch(event) {
     .finally(() => {
       form.reset();
       loader.classList.add('is-hidden');
+      btnLoad.classList.remove('is-hidden');
     });
 };
+
 function onFetchError(error) {
   alert(error);
 }
+
+function onLoad(event) {
+  pageValue = pageValue + 1;
+  fetchImg(searchValue)
+    .then(image => {
+      renderImageCard(image);
+    }) 
+    .catch (error => {
+      onFetchError(error);
+    })
+    .finally(() => {
+      
+    });
+};
 
 
