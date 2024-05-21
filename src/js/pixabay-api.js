7252d93f5a7a0d8fb60f43085716b67092a2f5af
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2024-05-21 16:37:27 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '43873427-52524e51f424820d3ed845203';
-const PER_PAGE = 4;
+const PER_PAGE = 15;
 
 export async function fetchImg(searchquery, page) {
     const params = `?key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${PER_PAGE}`;
